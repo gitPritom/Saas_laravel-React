802c5629ca561e3c700470ae51608676f193ce8a
pritom06 <pritomsaha20022018@gmail.com>
2026-03-08 14:31:47 +0530

    Spatie UUID issue solve

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,7 @@ class User extends Authenticatable
     protected $hidden = ['password', 'remember_token'];
 
     protected $casts = [
+        'id' => 'string',
         'email_verified_at' => 'datetime',
         'last_seen_at' => 'datetime',
         'password' => 'hashed',
